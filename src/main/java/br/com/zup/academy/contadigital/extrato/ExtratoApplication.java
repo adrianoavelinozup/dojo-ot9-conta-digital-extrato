916adab7dc5bae2f37517f9e6e2e35a6660c3118
Adriano Avelino <adriano.avelino@zup.com.br>
2021-12-07 18:11:18 -0300

    adiciona aplicação inicial do spring

---
+++ src/main/java/br/com/zup/academy/contadigital/extrato/ExtratoApplication.java
@@ -0,0 +1,13 @@
+package br.com.zup.academy.contadigital.extrato;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExtratoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExtratoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/zup/academy/contadigital/extrato/ExtratoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.zup.academy.contadigital.extrato;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExtratoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
